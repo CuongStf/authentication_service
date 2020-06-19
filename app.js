d8518e7b447b5ee80ae45da0285959590102c3b9
Cuongdevjs <nguyenmanhcuong.stf@gmail.com>
2020-06-19 13:45:29 +0700

    update endpoint

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get("/", (req, res, next) => {
   res.status(200).send("Successfully");
 });
 
-app.get("/test", (req, res, next) => {
+app.post("/test", (req, res, next) => {
   console.log(req.body);
   res.status(200).json({ data: "Successfully" });
 });
